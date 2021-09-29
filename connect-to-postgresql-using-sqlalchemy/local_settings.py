f8b44a9c9968ebcb3ff021134f4cbe498b1d4f3c
Leo Smigel <leo.smigel@gmail.com>
2021-09-28 22:22:05 -0400

    Added Connect to PostgreSQL Using SQLAlchemy post

---
+++ connect-to-postgresql-using-sqlalchemy/local_settings.py
@@ -0,0 +1,6 @@
+postgresql = {'pguser':'alpha',
+              'pgpasswd':'password',
+              'pghost':'localhost',
+              'pgport': 5432,
+              'pgdb': 'alpha'
+             }
