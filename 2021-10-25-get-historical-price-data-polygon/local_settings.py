ce00de470233fa2eadc0494cc191cf1338c72fcb
Leo Smigel <leo.smigel@gmail.com>
2021-10-25 22:29:10 -0400

    Added Get Historical Price Data from Polygon.io

---
+++ 2021-10-25-get-historical-price-data-polygon/local_settings.py
@@ -0,0 +1,11 @@
+polygon = {
+    'api_key':'YOUR_API_KEY_HERE'
+}
+
+postgresql = {'pguser':'alpha',
+              'pgpasswd':'password',
+              'pghost':'localhost',
+              'pgport': 5432,
+              'pgdb': 'alpha'
+             }
+
